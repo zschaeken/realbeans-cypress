6d7486ef0d4de7bc89799bdaea02de27657a2e88
Zjef Schaeken <r1036939@student.thomasmore.be>
2026-05-23 10:27:13 +0200

    Verwijder cy.session voor cloud compatibiliteit

--- cypress/e2e/realbeans.cy.js
+++ cypress/e2e/realbeans.cy.js
@@ -1,52 +1,51 @@
 const STORE_PASSWORD = 'shewbi'
 const BASE_URL = 'https://r1036939-realbeans.myshopify.com'
 
-// Sessie wordt eenmalig aangemaakt en hergebruikt door alle tests
-// cy.session() slaat cookies automatisch op na het inloggen
-const login = () => {
-  cy.session('shopify-login', () => {
-    cy.visit(BASE_URL)
-    cy.get('input[type="password"]').type(STORE_PASSWORD)
-    cy.get('button[type="submit"]').click()
-    cy.url().should('not.include', '/password')
+// Wachtwoord invullen als het scherm zichtbaar is
+const bypassPassword = () => {
+  cy.url().then((url) => {
+    if (url.includes('/password')) {
+      cy.get('input[type="password"]').type(STORE_PASSWORD)
+      cy.get('button[type="submit"]').click()
+      cy.url().should('not.include', '/password')
+    }
   })
 }
 
 describe('RealBeans webshop tests', () => {
 
-  beforeEach(() => {
-    login()
-  })
-
   it('homepage toont de introtekst', () => {
     cy.visit(BASE_URL)
-    cy.contains('Since 1801, RealBeans has roasted premium coffee').should('be.visible')
+    bypassPassword()
+    cy.contains('Since 1801, RealBeans has roasted premium coffee').should('exist')
   })
 
   it('homepage toont de producten', () => {
     cy.visit(BASE_URL)
-    cy.contains('Roasted coffee beans 5kg').should('be.visible')
-    cy.contains('Blended coffee 5kg').should('be.visible')
+    bypassPassword()
+    cy.get('main').contains('Roasted coffee beans 5kg').should('exist')
+    cy.get('main').contains('Blended coffee 5kg').should('exist')
   })
 
   it('About pagina toont de historietekst', () => {
     cy.visit(`${BASE_URL}/pages/about`)
-    cy.contains('From a small Antwerp grocery').should('be.visible')
+    bypassPassword()
+    cy.contains('From a small Antwerp grocery').should('exist')
   })
 
   it('productcatalog toont beide producten', () => {
-  cy.visit(`${BASE_URL}/collections/all`)
-  // Zoek specifiek in de productlijst, niet in het navigatiemenu
-  cy.get('.product-card, .card, main').contains('Roasted coffee beans 5kg').should('exist')
-  cy.get('.product-card, .card, main').contains('Blended coffee 5kg').should('exist')
-})
+    cy.visit(`${BASE_URL}/collections/all`)
+    bypassPassword()
+    cy.get('main').contains('Roasted coffee beans 5kg').should('exist')
+    cy.get('main').contains('Blended coffee 5kg').should('exist')
+  })
 
-it('productdetailpagina toont juiste info', () => {
-  cy.visit(`${BASE_URL}/collections/all`)
-  // force: true omdat de productafbeelding de link bedekt
-  cy.get('main').contains('Roasted coffee beans 5kg').click({ force: true })
-  cy.contains('Our best and sustainable real roasted beans').should('exist')
-  cy.contains('$40').should('exist')
-})
+  it('productdetailpagina toont juiste info', () => {
+    cy.visit(`${BASE_URL}/collections/all`)
+    bypassPassword()
+    cy.get('main').contains('Roasted coffee beans 5kg').click({ force: true })
+    cy.contains('Our best and sustainable real roasted beans').should('exist')
+    cy.contains('$40').should('exist')
+  })
 
 })
