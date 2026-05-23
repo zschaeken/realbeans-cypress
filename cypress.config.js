d611856e14150a078423e3ebe5bf90896ca32060
Zjef Schaeken <r1036939@student.thomasmore.be>
2026-05-23 10:33:04 +0200

    Gebruik Chrome browser en geheugenoptimalisatie

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  // Koppeling met Cypress Cloud project
   projectId: 'm2x5qi',
+  
+  // Minder tests in geheugen houden om crashes te vermijden
+  numTestsKeptInMemory: 1,
+  experimentalMemoryManagement: true,
 
   e2e: {
     baseUrl: 'https://r1036939-realbeans.myshopify.com',
